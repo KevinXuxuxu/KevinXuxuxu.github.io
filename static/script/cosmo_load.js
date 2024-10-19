9f1e77eef98d3d7e85a56e1006a36eafb9a53612
KevinXuxuxu <kevin.xu.fangzhou@gmail.com>
2024-10-18 19:24:28 -0700

    update

--- static/script/cosmo_load.js
+++ static/script/cosmo_load.js
@@ -2,22 +2,30 @@ import init, { PlayerWASM } from '/static/wasm/cosmo/cosmo.js';
 await init();
 async function readScene(id) {
     try {
+        console.log('debug: 5')
         const response = await fetch('/static/cosmo_scenes/' + id + '.cos');
         if (!response.ok) {
+            console.log('debug: 8')
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const text = await response.text();
+        console.log('debug: 12')
         return text.split('\n').map(line => line.trim());
     } catch (error) {
+        console.log('debug: 15')
         console.error('Error fetching or reading the text file:', error);
     }
 }
 async function prepareCosmo(displayEle) {
+    console.log('debug: 20')
     let [id, w, h] = displayEle.id.split('-');
     const scene = await readScene(id);
+    console.log('debug: 23')
     const player = PlayerWASM.new(scene, 24, parseInt(w), parseInt(h), false, false, false);
+    console.log('debug: 25')
     player.update();
     displayEle.textContent = player.get_a().join('\n');
+    console.log('debug: 28')
     displayEle.addEventListener('click', () => {
         if (displayEle.hasAttribute('intId')) {
             clearInterval(displayEle.getAttribute('intId'));
@@ -33,8 +41,10 @@ async function prepareCosmo(displayEle) {
 }
 
 window.onload = function () {
+    console.log('debug: 44')
     let displayEles = document.getElementsByClassName('cosmo-display');
     for (let i = 0; i < displayEles.length; ++i) {
+        console.log('debug: 47')
         prepareCosmo(displayEles[i]);
     }
 };
